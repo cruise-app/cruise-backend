708d2264a2aa18b540e3162e5f700e69b267df9b
3omdawy11 <mohamedzaky970@gmail.com>
2025-03-09 00:49:52 +0200

    API connected to bloc

--- routers/user_router.js
+++ routers/user_router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const UserController = require("../controllers/user_controller");
 router.post("/register", UserController.registerUser);
-router.post("/check-email", UserController.checkEmail);
-router.post("/check-phoneNumber", UserController.checkPhoneNumber);
+router.post("/verify-email", UserController.checkEmail);
+router.post("/verify-phoneNumber", UserController.checkPhoneNumber);
 router.post("/verify-otp", UserController.verifyOTP);
 
 module.exports = router;
